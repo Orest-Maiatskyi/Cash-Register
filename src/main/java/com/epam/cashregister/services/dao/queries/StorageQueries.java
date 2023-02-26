011363296445f6628bc112f63f392b81220caaa3
orest <orestkiev@gmail.com>
2023-02-26 21:49:34 +0200

    new sql queries have been added to manipulate storages

--- src/main/java/com/epam/cashregister/services/dao/queries/StorageQueries.java
+++ src/main/java/com/epam/cashregister/services/dao/queries/StorageQueries.java
@@ -4,5 +4,6 @@ public class StorageQueries {
 
     public static String selectStorageByAddress = "SELECT * FROM storages WHERE address = ?";
     public static String selectAllStorages = "SELECT * FROM storages";
-
+    public static String insertIntoStorages = "INSERT INTO storages (address) VALUES (?)";
+    public static String deleteStorages = "DELETE FROM storages WHERE (address=?)";
 }
